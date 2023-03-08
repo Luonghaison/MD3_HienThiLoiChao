f62b6d11200494b3c961f9b987d262bc21c7c9b4
unknown <keibin.wru@gmail.com>
2023-03-08 08:16:01 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner  sc = new Scanner(System.in);
+        System.out.println("Nhập tên của bạn:");
+        String a = sc.nextLine();
+        System.out.println("Chào mừng bạn "+ a);
+    }
+}
